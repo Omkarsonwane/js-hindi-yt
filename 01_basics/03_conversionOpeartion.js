e8718ac5673d4abc0a4d3ec1924925bc58f73979
Omkarsonwane <151514189+Omkarsonwane@users.noreply.github.com>
2025-09-06 07:20:43 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOpeartion.js
@@ -0,0 +1,29 @@
+let score ="33ad"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+ 
+// "33" => 33
+// "33wsed" => NaN
+// true => 1 ; false => 0
+//
+
+let isLoggedIn = ""
+let booleanisLoggedIn = Boolean (isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true ; 0 => false 
+// "" => false 
+// "omkar" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
