cc18902cab370bc6f4b03dd39309de572d9f1e0d
Omkarsonwane <151514189+Omkarsonwane@users.noreply.github.com>
2025-09-06 08:04:54 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// comparison and equality work diffrently in js
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+// ===  :strictly equal to
+
+console.log("2" === 2);
